40522aeda9feb9d008a01f22c51f2bdc585921d4
TioRAC <tiorac@msn.com>
2021-05-23 21:31:59 -0300

    script generate videos url

---
+++ generateVideos.js
@@ -0,0 +1,24 @@
+const glob = require("glob")
+
+var args = process.argv.slice(2)
+console.log('Arguments: ', args)
+
+var getDirectories = function (src, callback) {
+    glob(src + '/**/*', { nodir: true }, callback)
+};
+
+getDirectories(args[0], function (err, res) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+        formartUri(res)
+    }
+});
+
+formartUri = (files) => {
+    const uriFiles = files.map(f => f.replace(args[0], args[1]))
+    const json = JSON.stringify(uriFiles, null, '\t')
+    const fs = require('fs')
+
+    fs.writeFile('videos.json', json, 'utf8', () => { console.log("End...") })
+}
